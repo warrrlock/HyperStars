71d56a811aabe975ff4d713a939e29d585171cde
Steven <yh3133@nyu.edu>
2022-10-12 12:56:22 -0400

    steve scene optimizations

--- Assets/SFX/PostWwiseEvent.cs
+++ Assets/SFX/PostWwiseEvent.cs
@@ -11,27 +11,6 @@ public class PostWwiseEvent : MonoBehaviour
         comboEvents[index].Post(gameObject);
     }
 
-    public void PlayComboSound2()
-    {
-        comboEvents[1].Post(gameObject);
-    }
-
-    public void PlayComboSound3()
-    {
-        comboEvents[2].Post(gameObject);
-    }
-
-    public void PlayComboSound4()
-    {
-        comboEvents[3].Post(gameObject);
-    }
-
-    public void PlayComboSound5()
-    {
-        comboEvents[4].Post(gameObject);
-    }
-    
-    // Update is called once per frame.
     void Update()
     {
     
