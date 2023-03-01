d5e4b92ef3fbe5e10476156870724956af2f07a9
Steve Hu <yh3133@nyu.edu>
2023-03-01 18:19:03 -0500

    sound manager rework

--- Assets/SFX/PostWwiseEvent.cs
+++ Assets/SFX/PostWwiseEvent.cs
@@ -3,73 +3,77 @@ using System.Collections.Generic;
 //using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 
-public enum Wwise_ComboEvents
-{
-    COMBO1, COMBO2, COMBO3, COMBO4, COMBO5, SPECIAL_NEUTRAL, SPECIAL_SIDE, AIR_LIGHT, AIR_MEDIUM, AIR_SPECIAL,
-    LOW_LIGHT, LOW_MEDIUM, LOW_SPECIAL
-}
-
 public enum Wwise_MovementEvents
 {
     FOOTSTEPS, JUMP, DASH, LAND, CROUCH
 }
 
+public enum Wwise_SpecialSwings
+{
+    
+}
+
 public class PostWwiseEvent : MonoBehaviour
 {
     private Fighter _fighter;
+    [Header("Set to character")]
     public AK.Wwise.Switch CharacterSwitch;
-    public AK.Wwise.Event[] attackEvents;
-    public AK.Wwise.Event[] movementEvents;
-    [Header("Environment Bounces")]
-    public AK.Wwise.Event wallBounceEvent;
-    public AK.Wwise.Event hurtLandEvent;
-    [Header("Hits")]
-    public AK.Wwise.Event lightHit;
-    public AK.Wwise.Event meidumHit;
-    public AK.Wwise.Event heavyHit;
-    
-    public AK.Wwise.Event blockEvent;
 
-    // public AK.Wwise.Event comboVoicelineEvent;
+    [Header("Specials")]
+    public AK.Wwise.Event[] specialSwings;
+    [Header("Movements")]
+    public AK.Wwise.Event[] movements;
 
     private void Start()
     {
-        // set cahracter swtich
+        // set character switch
         CharacterSwitch.SetValue(gameObject);
-        
+        // get fighter
         _fighter = GetComponent<Fighter>();
+        // hits
+        _fighter.Events.onAttackHit += Wwise_PlayHitSound;
+        // blocks
+        _fighter.Events.onBlockHit += ctx => Wwise_PlaySingle(SoundManagerCombat.Instance.blockHit);
         // environment hits
-        _fighter.Events.wallBounce += () => Wwise_PlaySingle(wallBounceEvent);
-        _fighter.Events.onLandedHurt += () => Wwise_PlaySingle(hurtLandEvent);
+        _fighter.Events.wallBounce += () => Wwise_PlaySingle(SoundManagerCombat.Instance.environmentHits[(int)Wwise_CombatEnvironmentSounds.WallBounce]);
+        _fighter.Events.onLandedHurt += () => Wwise_PlaySingle(SoundManagerCombat.Instance.environmentHits[(int)Wwise_CombatEnvironmentSounds.GroundSplat]);
         // land
-        _fighter.Events.onLandedNeutral += () => Wwise_PlaySingle(movementEvents[(int)Wwise_MovementEvents.LAND]);
-        // hit
-        _fighter.Events.onAttackHit += Wwise_PlayHit;
-        // block
-        _fighter.Events.onBlockHit += Wwise_PlayBlock;
-    }
-
-    private void Update()
-    {
-        
+        _fighter.Events.onLandedNeutral += () => Wwise_PlaySingle(movements[(int)Wwise_MovementEvents.LAND]);
     }
     
-    public void Wwise_PlayAttackSound(Wwise_ComboEvents cEvent)
-    {
-        attackEvents[(int)cEvent].Post(gameObject);
-    }
-
-    public void Wwise_PlayMovementSound(Wwise_MovementEvents mEvent)
-    {
-        movementEvents[(int)mEvent].Post(gameObject);
-    }
-
     private void Wwise_PlaySingle(AK.Wwise.Event e)
     {
         e.Post(gameObject);
     }
+    
+    /// <summary>
+    /// Play swing sound in animation
+    /// </summary>
+    /// <param name="swings"></param>
+    public void Wwise_PlayAttackSwing(Wwise_CombatSwings swings)
+    {
+        SoundManagerCombat.Instance.swingEvents[(int)swings].Post(gameObject);
+    }
 
-    private void Wwise_PlayHit(Dictionary<string, object> message)
+    /// <summary>
+    /// Play special swing in animation
+    /// </summary>
+    /// <param name="swings"></param>
+    public void Wwise_PlaySpecialSwing(Wwise_SpecialSwings swings)
+    {
+        specialSwings[(int)swings].Post(gameObject);
+    }
+
+    /// <summary>
+    /// Play movement in animation
+    /// </summary>
+    /// <param name="mEvent"></param>
+    public void Wwise_PlayMovementSound(Wwise_MovementEvents mEvent)
+    {
+        movements[(int)mEvent].Post(gameObject);
+    }
+    
+    private void Wwise_PlayHitSound(Dictionary<string, object> message)
     {
         
         //Play Character Hit Sound
@@ -78,23 +82,17 @@ public class PostWwiseEvent : MonoBehaviour
         switch (type)
         {
             case AttackInfo.AttackType.Light:
-                lightHit.Post(receiver.gameObject);
+                SoundManagerCombat.Instance.hitEvents[(int)Wwise_CombatHits.Light].Post(receiver.gameObject);
                 break;
             case AttackInfo.AttackType.Medium:
-                meidumHit.Post(receiver.gameObject);
+                SoundManagerCombat.Instance.hitEvents[(int)Wwise_CombatHits.Medium].Post(receiver.gameObject);
                 break;
-            case AttackInfo.AttackType.Special:
-                heavyHit.Post(receiver.gameObject);
+            case AttackInfo.AttackType.Heavy:
+                SoundManagerCombat.Instance.hitEvents[(int)Wwise_CombatHits.Heavy].Post(receiver.gameObject);
                 break;
         }
     }
     
-    private void Wwise_PlayBlock(Dictionary<string, object> message)
-    {
-        blockEvent.Post(gameObject);
-    }
-    
-        
     static uint[] playingIds = new uint[50];
     public bool IsEventPlayingOnGameObject(string eventName)
     {
