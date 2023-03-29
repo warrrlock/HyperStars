a6606fb85dc6bca300f4cf5a36c42d3db0600181
Steven <yh3133@nyu.edu>
2023-03-29 16:16:50 -0400

    added color picker v1

---
+++ Assets/VFX/CharacterColors/CharacterColorScriptable.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character/CharacterColors")]
+public class CharacterColorScriptable : ScriptableObject
+{
+    [field: SerializeField] public ColorSix[] LisaColors { private set; get; }
+}
+
+[Serializable] public class ColorSix
+{
+    [field: SerializeField] public Color Color1 { private set; get; }
+    [field: SerializeField] public Color Color2 { private set; get; }
+    [field: SerializeField] public Color Color3 { private set; get; }
+    [field: SerializeField] public Color Color4 { private set; get; }
+    [field: SerializeField] public Color Color5 { private set; get; }
+    [field: SerializeField] public Color Color6 { private set; get; }
+}
