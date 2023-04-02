76de48bb9365d2dcdbc0ddcaf20f291036748374
StevoPineapple <new.logintesting@gmail.com>
2023-04-02 17:17:22 -0400

    !Moved everything from BlockoutStuff to StageAssets, you might redownload everything again. I hope this doesn't blow everything up. Deleted some unsed stuff. Organized folders.

---
+++ Assets/StageAssets/ObjectJumpy.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectJumpy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool randomize;
+    public bool startWithHeight;
+    public bool onPlat;
+    public CrowdManager crowdManager;
+
+    public float minSpeed;
+    public float maxSpeed;
+    public float maxHeight;
+    float speed;
+    float height;
+    private float initHeight;
+    private float jumpHeight;
+    public Transform platTrans;
+    private float platHeight;
+    private float platInitHeight;
+
+    private MusicManager musicMan;
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        crowdManager = GameObject.Find("CrowdManager").GetComponent<CrowdManager>();
+        musicMan = crowdManager.musicManager;
+        RandomizeColor();
+
+        if (onPlat)
+        {
+            platInitHeight = platTrans.position.y;
+        }
+        if (startWithHeight)
+            initHeight = transform.position.y;
+        if (randomize)
+        {
+            speed = 3 + Random.Range(minSpeed, maxSpeed);
+            height = Random.Range(0, maxHeight);
+        }
+        else
+            speed = maxSpeed;
+            height = maxHeight;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (musicMan.Impressed)
+            jumpHeight = Mathf.Abs(height * 1.8f * Mathf.Sin(speed * 1.8f * Time.realtimeSinceStartup));
+        else
+            jumpHeight = Mathf.Abs(height * Mathf.Sin(speed * Time.realtimeSinceStartup));
+
+        if (onPlat)
+        {
+            platHeight = platTrans.position.y;
+            transform.position = new Vector3(transform.position.x, initHeight+jumpHeight + (platHeight - platInitHeight), transform.position.z);
+        }
+        else
+            transform.position = new Vector3(transform.position.x, initHeight+jumpHeight, transform.position.z);
+    }
+    void RandomizeColor()
+    {
+        SpriteRenderer sprRend = gameObject.GetComponent<SpriteRenderer>();
+        int sprIndex = Random.Range(0, crowdManager.sprListLength - 1);
+        sprRend.sprite = crowdManager.sprList[sprIndex];
+        float H, S, V;
+        Color col = sprRend.color;
+        Color.RGBToHSV(col, out H, out S, out V);
+        sprRend.color = Color.HSVToRGB(Random.Range(0.0f,1.0f),S,V);
+    }
+}
