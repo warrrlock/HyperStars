724357d9d50febb2969753c952c3027093fdee2c
jade <jw5933@nyu.edu>
2023-04-11 15:58:38 -0400

    Projectile SFX

--- Assets/Scripts/Attacks/ProjectileAction.cs
+++ Assets/Scripts/Attacks/ProjectileAction.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public abstract class ProjectileAction: ScriptableObject
 {
+    [Tooltip("what this action should do when executed.")]
     public abstract void Execute(Projectile projectile);
+    [Tooltip("what the action does when the projectile is stopped/destroyed. Not implemented yet.")]
     public abstract void Stop(Projectile projectile);
 }
